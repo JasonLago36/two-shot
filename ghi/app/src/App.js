b136d937f560dd1fe8f585ec04c67adadf4302ff
MasterCraftArc <mikeslaughter79@gmail.com>
2023-04-19 22:16:02 -0600

    semi finished frontEnd

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-
+import ShoesForm from './ShoesForm';
+import ShoesList from './ShoesList'
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+        </Routes>
+        <Routes>
+          <Route path="shoes/" element={<ShoesForm />} />
+          <Route path="shoes/list" element={<ShoesList/>} />
         </Routes>
       </div>
     </BrowserRouter>
